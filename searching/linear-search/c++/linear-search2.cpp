14e218f1d808efa3346946cfc900c3cc530233aa
slowed <rootslowed@gmail.com>
2021-06-21 09:22:45 -0300

    repo update

--- searching/linear-search/c++/linear-search2.cpp
+++ searching/linear-search/c++/linear-search2.cpp
@@ -7,10 +7,8 @@ void search(vector<int> arr, int search_Element)
   int length = arr.size();
   int position = -1;
   int right = length - 1;
-
   for (left = 0; left <= right;)
   {
-
     if (arr[left] == search_Element)
     {
       position = left;
@@ -19,7 +17,6 @@ void search(vector<int> arr, int search_Element)
            << left + 1 << " Attempt";
       break;
     }
-
     if (arr[right] == search_Element)
     {
       position = right;
@@ -28,20 +25,16 @@ void search(vector<int> arr, int search_Element)
            << length - right << " Attempt";
       break;
     }
-
     left++;
     right--;
   }
-
   if (position == -1)
     cout << "Not found in Array with "
          << left << " Attempt";
 }
-
 int main()
 {
   vector<int> arr{1, 2, 3, 4, 5};
   int search_element = 5;
-
   search(arr, search_element);
 }
