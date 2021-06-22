c51a2e97c73c35022202a4c39f9ba9f70f800882
slowed <rootslowed@gmail.com>
2021-06-22 20:28:26 -0300

    repo update to c++ only

--- searching/binary-search/binary-search3.cpp
+++ searching/binary-search/binary-search3.cpp
@@ -6,12 +6,9 @@ int binarySearch(int arr[], int l, int r, int x)
   while (l <= r)
   {
     int m = l + (r - l) / 2;
-    if (arr[m] == x)
-      return m;
-    if (arr[m] < x)
-      l = m + 1;
-    if (arr[m] > x)
-      r = m - 1;
+    if (arr[m] == x) return m;
+    if (arr[m] < x) l = m + 1;
+    if (arr[m] > x) r = m - 1;
   }
   return -1;
 }
@@ -22,7 +19,8 @@ int main(void)
   int x = 10;
   int n = sizeof(arr) / sizeof(arr[0]);
   int result = binarySearch(arr, 0, n - 1, x);
-  (result == -1) ? cout << "Element is not present in array"
-                 : cout << "Element is present at index " << result;
+  (result == -1)
+    ? cout << "Element is not present in array"
+    : cout << "Element is present at index " << result;
   return 0;
 }
